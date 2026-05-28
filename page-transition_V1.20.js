e8ea5b5e3397b6bce644fae13f256452ae86f77e
AVOSSSigmaTeam <113782378+AVOSSSigmaTeam@users.noreply.github.com>
2026-05-28 19:28:46 +0200

    Update and rename page-transition_V1.19.js to page-transition_V1.20.js

--- page-transition_V1.20.js
+++ page-transition_V1.20.js
@@ -739,10 +739,11 @@ function scrollToPendingAnchor(container) {
   if (lenis && typeof lenis.scrollTo === "function") {
     lenis.scrollTo(target, {
       offset,
-      duration: reducedMotion ? 0 : 5,
+      // duration: reducedMotion ? 0 : 5,
       // lerp: reducedMotion ? 1 : lenisLerpValue,
       // easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      immediate: reducedMotion,
+      // immediate: reducedMotion,
+      immediate: true,
       force: true
     });
   } else {
